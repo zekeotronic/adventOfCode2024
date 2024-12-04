8dd461934b83f7f509b9de2e9388f4e56eb7f04d
Eric Ihle <zekeotronic@gmail.com>
2024-12-04 14:28:23 -0500

    cleans up day one

--- dayOne/01-day-one.ts
+++ dayOne/01-day-one.ts
@@ -1,18 +1,23 @@
-type Lines = string[] | string[][];
-let lines : Lines;
 function findDistance(left : number, right : number) : number {
     let distance = 0;
     left > right ? distance = left - right : distance = right - left;
     return distance;
 }
+
 const input = await Deno.readTextFile("one-input.txt");
-lines = input.split("\n").map((line) => line.trim()).map((line) => line.split('   '));
+
+const lines = input.split("\n").map((line) => line.trim()).map((line) => line.split('   '));
+
 const leftList = lines.map((line) => +line[0]).sort((a, b) => a - b);
+
 const rightList = lines.map((line) => +line[1]).sort((a, b) => a - b);
+
 const distances = [];
+
 for (let i = 0; i < leftList.length; i++) {
     distances.push(findDistance(leftList[i], rightList[i]));
 }
+
 const totalDistance = distances.reduce((acc, curr) => acc + curr, 0);
 
 console.log(totalDistance)
